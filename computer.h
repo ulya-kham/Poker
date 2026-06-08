f7c2d0f30a44c33193857128fbe5bf273e329982
Ul_ka <ulya.xamzina@inbox.ru>
2026-06-08 15:57:48 +0500

    Delete computer.h

--- computer.h
+++
@@ -1,7 +0,0 @@
-// computer.h
-#pragma once
-
-#include "karta.h"
-
-// ѕринимает решение: сколько ставить (0 = пас)
-int reshenieComputera(int fichkiComp, int minStavka, karta rukaKaz[2], karta stol[5], int etap, int fichkiIgroka);
